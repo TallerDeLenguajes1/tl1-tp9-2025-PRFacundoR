5332de206b9f35804eb498773939e11e97207f13
FacundoPeralta <facundoperalta217@gmail.com>
2025-06-13 19:23:47 -0300

    terminado punto 1

--- LectorDirectorio/manipulacion.cs
+++ LectorDirectorio/manipulacion.cs
@@ -19,8 +19,10 @@ namespace LectorDirectorio
         {
 
             List<string> ListadoDeCarpetas = Directory.GetDirectories(direccion).ToList();
+            List<string> ListadoDeArchivo = Directory.GetFiles(direccion).ToList();
 
-            List<string> ListadoDeArchivo= Directory.GetFiles(direccion).ToList();
+
+
 
             foreach (string carpeta in ListadoDeCarpetas)
             {
@@ -30,19 +32,37 @@ namespace LectorDirectorio
 
 
             }
+
+            string ruta = "reporte_archivos.csv";
+            if (!File.Exists(ruta))
+            {
+                File.WriteAllText(ruta, "Nombre del Archivo;Tamaño (KB);Fecha de Última Modificación"+ Environment.NewLine);// crea y escrive dentro del archivp
+
+            }
+
             foreach (string archivo in ListadoDeArchivo)
             {
                 Console.WriteLine(archivo);
+
                 FileInfo infoArchivo = new FileInfo(archivo);
+
                 long archivoKB = infoArchivo.Length / 1024;
-                Console.WriteLine("El peso del archivo es :" +archivoKB + " KB");
-                
+                Console.WriteLine("El peso del archivo es :" + archivoKB + " KB");
+
+
+                string nombreArchivo = Path.GetFileName(archivo);
+                string fechaModificacion = infoArchivo.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
+                string lineaCSV = $"{nombreArchivo};{archivoKB};{fechaModificacion}";
+
+                File.AppendAllText(ruta, lineaCSV+Environment.NewLine/*sato de linea*/);
+
+
             }
 
-            
+
         }
 
-    
+
 
 
 
